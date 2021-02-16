83610b8d1745635bc9cb8e9eb514b0d889224274
RMondin <richardmondin@hotmail.com>
2021-02-15 21:12:52 -0300

    Richard - Tela Owner First Enter

--- lib/OwnerApp/views/mainPage/overviewPage.dart
+++ lib/OwnerApp/views/mainPage/overviewPage.dart
@@ -17,12 +17,59 @@ class _OverViewPageState extends State<OverViewPage> {
         width: MediaQuery.of(context).size.width,
         child: Padding(
           padding: const EdgeInsets.only(bottom: 12.0),
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: RaisedButton(
-              onPressed: () => print("x"),
-              child: Text("Adicionar Novo Local"),
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 6,
+                child: ListView.builder(
+                  itemCount: 2,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Card(
+                        child: Container(
+                          height: 100,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(2.0),
+                                    child: ClipOval(
+                                      child: Container(
+                                        decoration:
+                                            BoxDecoration(color: Colors.red),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 3,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text("INFO"),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: RaisedButton(
+                  onPressed: () => print("x"),
+                  child: Text("Adicionar Novo Local"),
+                ),
+              ),
+            ],
           ),
         ),
       ),
