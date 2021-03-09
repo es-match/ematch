45182139998d4a931cd3a7ecf85ad1316781e848
RMondin <richardmondin@hotmail.com>
2021-03-08 23:20:50 -0300

    Richard - Implementaçao groupDetailsPage

--- lib/App/view/UserViews/group/groupDetailsPage.dart
+++ lib/App/view/UserViews/group/groupDetailsPage.dart
@@ -15,8 +15,122 @@ class _GroupDetailsPageState extends State<GroupDetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        height: 100,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          buildTopImageWidget(context),
+          buildDescription(),
+          Expanded(
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(bottom: 1, left: 8, right: 8, top: 3),
+              child: Column(
+                children: [
+                  Text("Próximas Partidas"),
+                  Flexible(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                        ),
+                      ),
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: 10,
+                        itemBuilder: (context, index) => ListTile(
+                          title: Container(
+                            height: 80,
+                            width: MediaQuery.of(context).size.width,
+                            child: Card(
+                                shape: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                      color: Colors.black.withOpacity(0.5),
+                                      width: 1),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(6.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                          flex: 9,
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceAround,
+                                            children: [
+                                              Row(
+                                                children: [
+                                                  Icon(Icons
+                                                      .calendar_today_outlined),
+                                                  Text("08/12, 19:00 - 21:00")
+                                                ],
+                                              ),
+                                              Align(
+                                                  alignment:
+                                                      Alignment.centerLeft,
+                                                  child: Text(
+                                                      "Paintball World - 5 km")),
+                                            ],
+                                          )),
+                                      Expanded(
+                                          flex: 3,
+                                          child: Container(
+                                              child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Icon(Icons.check_circle_outline,
+                                                  color: Colors.green),
+                                              Text("Confirmado")
+                                            ],
+                                          ))),
+                                    ],
+                                  ),
+                                )),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: buildBottomButtons(context),
+    );
+  }
+
+  Padding buildDescription() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Descrição"),
+          TextField(
+            textInputAction: TextInputAction.done,
+            maxLines: 5,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Stack buildTopImageWidget(BuildContext context) {
+    return Stack(children: [
+      Container(
+        height: 150,
         width: MediaQuery.of(context).size.width,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -26,6 +140,42 @@ class _GroupDetailsPageState extends State<GroupDetailsPage> {
           ),
         ),
       ),
+      Container(
+        height: 150,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              widget.group.groupName,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    ]);
+  }
+
+  Column buildBottomButtons(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: RaisedButton(
+              child: Text('Agendar uma Nova Partida'), onPressed: () => {}
+              //  Navigator.push(
+              // context,
+              // MaterialPageRoute(builder: (context) => ()),
+              ),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: RaisedButton(
+              child: Text('Ver Participantes do Grupo'), onPressed: () => {}),
+        ),
+      ],
     );
   }
 }
