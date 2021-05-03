3b0ac952716e80dc85f4396756d6bdb13e2ffa3c
acisotto <61620892+acisotto@users.noreply.github.com>
2021-05-03 20:42:41 -0300

    Alvaro - Payment Rep e Controller

--- lib/App/controller/mercadopagoService.dart
+++ lib/App/controller/mercadopagoService.dart
@@ -2,23 +2,25 @@ import 'package:ematch/App/model/locationModel.dart';
 import 'package:flutter/material.dart';
 import 'package:mercado_pago_mobile_checkout/mercado_pago_mobile_checkout.dart';
 import 'package:ematch/App/model/paymentModel.dart';
+import 'package:ematch/App/repository/paymentRepository.dart';
 
 const publicKey = "TEST-0362a415-974e-4498-899e-290f3889ee73";
-const preferenceId = "161144975-bc528f33-4ccc-42c4-8ecb-90dfde5bf084";
 
 Future<String> createBillMP(PaymentModel payment, BuildContext context) async {
   try {
+    PaymentRepository repository = PaymentRepository();
     //https://api.mercadopago.com/checkout/preferences?access_token=TEST-3670125385404362-050218-f57af0447a1ea01927456c73d87174a9-161144975
-    return "161144975-bc528f33-4ccc-42c4-8ecb-90dfde5bf084";
+    String preferenceId = await repository.getPaymentBill(payment);
+    return preferenceId;
   } catch (error) {
     return null;
   }
 }
 
-Future<PaymentResult> paymentMP(String preferenceID) async {
+Future<PaymentResult> paymentMP(String preferenceId) async {
   PaymentResult result = await MercadoPagoMobileCheckout.startCheckout(
     publicKey,
-    preferenceID,
+    preferenceId,
   );
   return result;
 }
---
+++ lib/App/repository/paymentRepository.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:ematch/App/model/paymentModel.dart';
+import 'package:http/http.dart';
+
+class PaymentRepository {
+  final String url =
+      "https://api.mercadopago.com/checkout/preferences?access_token=TEST-3670125385404362-050218-f57af0447a1ea01927456c73d87174a9-161144975";
+
+  Future<String> getPaymentBill(PaymentModel paymentDetails) async {
+    var _body = paymentDetails.toJsonBillMP();
+
+    // String path =
+    //     "http://localhost:5001/esmatch-ce3c9/us-central1/dbGroups/api/v1/groups/";
+    final response = await post(
+      url,
+      // path,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: _body,
+    );
+
+    print(response.statusCode);
+    Map<String, dynamic> l;
+    if (response.statusCode == 200) {
+      l = json.decode(response.body);
+    }
+    return l['preferenceId'].toString();
+  }
+}
